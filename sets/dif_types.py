c68854ff7f2c76c1cfbd196006881debc5a111d0
Reginah24 <r.majura@alustudent.com>
2025-03-12 10:01:57 +0200

    get

---
+++ sets/dif_types.py
@@ -0,0 +1,4 @@
+#!/usr/hp/AppData/local/Microsoft/WindowsApps/python
+set_temp = {3,6,"hello",4.55,((10,6))}
+print(set_temp)
+#this set shows that a set can have all kinds of data types and still have an output
